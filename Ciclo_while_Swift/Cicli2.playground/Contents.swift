bda141605902d9e95b30a443ceefaf999fcec5d7
SviluppoCodice <51750512+Sviluppocodice@users.noreply.github.com>
2019-07-25 15:49:00 +0200

    Add files via upload

---
+++ Ciclo_while_Swift/Cicli2.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var contatore = 5
+
+var a = 0
+
+while contatore >= 0 {
+    
+    print("Ciao")
+    contatore -= 1
+    
+    a = contatore
+}
+
+print(a)
